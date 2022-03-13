80dee53e220b8050c46588677a77184a232de7e9
fanlumaster <1730976608@qq.com>
2022-03-13 19:25:39 +0800

    leet77.

---
+++ leet77/solu1.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.res = []
+        self.curRes = []
+
+        def backtrack(start: int, n: int, k: int):
+            if (len(self.curRes) == k):
+                self.res.append(self.curRes.copy())
+            for i in range(start, n + 1):
+                self.curRes.append(i)
+                backtrack(i + 1, n, k)
+                self.curRes.pop()
+
+        backtrack(1, n, k)
+        return self.res
+
+
+if __name__ == '__main__':
+    solu = Solution()
+    print(solu.combine(4, 2))
