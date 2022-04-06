be2fe26ed455eb5dca55bf3b4d106f19873ec811
fanyfull <lxyfl6688@gmail.com>
2022-04-06 20:13:25 +0800

    leet316.

---
+++ Python/leet316/solu1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        res = []
+        isAdded = {}
+        myDict = {}
+        for i in range(len(s)):
+            isAdded[s[i]] = False
+            if s[i] in myDict:
+                myDict[s[i]] += 1
+            else:
+                myDict[s[i]] = 1
+        for i in range(len(s)):
+            if len(res) == 0:
+                res.append(s[i])
+                isAdded[s[i]] = True
+                myDict[s[i]] -= 1
+            elif isAdded[s[i]] == False:
+                while len(res) != 0 and s[i] < res[-1] and myDict[res[-1]] > 0:
+                    tmp = res.pop()
+                    isAdded[tmp] = False
+                res.append(s[i])
+                isAdded[s[i]] = True
+                myDict[s[i]] -= 1
+            else:
+                myDict[s[i]] -= 1
+        res = ''.join(res)
+        return res
+
+
+solu = Solution()
+s = "bcabc"
+s = "cbacdcbc"
+s = "bbcaac"
+res = solu.removeDuplicateLetters(s)
+print(res)
---
+++ Python/leet316/testStrCompare.py
@@ -0,0 +1,8 @@
+str1 = 'a'
+str2 = 'b'
+print(str1 > str2)
+
+myStr = ['a', 'b', 'c', 'd']
+myStr = ''.join(myStr)
+print(myStr)
+print(myStr[::-1])
