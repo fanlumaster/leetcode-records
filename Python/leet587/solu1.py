3ca62f06294471a82537a79e45d9e44a48d547b4
fanlumaster <1730976608@qq.com>
2022-06-14 22:59:41 +0800

    leet427 初稿。

---
+++ Python/leet587/solu1.py
@@ -0,0 +1,11 @@
+'''
+Graham 算法
+'''
+
+from typing import List
+
+
+class Solution:
+    def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
+        def cross(p: List[int], q: List[int], r: List[int]) -> int:
+            return (q[0] - p[0])
