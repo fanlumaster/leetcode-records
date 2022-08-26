a89ec54a8d96e36f7149ac9b3b5a26818cbdd511
fanlumaster <1730976608@qq.com>
2022-08-27 04:42:34 +0800

    :)

---
+++ CSharp/CSharp/src/leet108/Solution.cs
@@ -0,0 +1,30 @@
+﻿using CSharp.src.utils;
+
+namespace CSharp.src.leet108
+{
+    public class Solution
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            List<int> numsList = new List<int>(nums);
+            TreeNode res = recurse(numsList);
+
+            return res;
+        }
+
+        private TreeNode recurse(List<int> numsList)
+        {
+            if (numsList.Count == 0)
+            {
+                return null;
+            }
+            int mid = numsList.Count / 2;
+            TreeNode curNode = new TreeNode(numsList[mid]);
+            List<int> leftSubList = numsList.GetRange(0, mid);
+            List<int> rightSubList = (mid >= numsList.Count - 1) ? new List<int>() : numsList.GetRange(mid + 1, numsList.Count - mid - 1);
+            curNode.left = recurse(leftSubList);
+            curNode.right = recurse(rightSubList);
+            return curNode;
+        }
+    }
+}
---
+++ CSharp/CSharpTests/src/leet108/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using CSharp.src.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.src.leet108.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void SortedArrayToBSTTest()
+        {
+            var solu = new Solution();
+            var data = new List<int?> { 0, -3, 9, -10, null, 5 };
+            var root = BinaryTreeUtils.BuildBinTreeFromLevelOrder(data);
+            BinaryTreeUtils.PrintBinTree(root);
+            Console.WriteLine("============================");
+            var nums = new int[] { -10, -3, 0, 5, 9 };
+            var res = solu.SortedArrayToBST(nums);
+            BinaryTreeUtils.PrintBinTree(res);
+        }
+
+        [TestMethod()]
+        public void TestList()
+        {
+            var list = new List<int>();
+            Console.WriteLine(list.Count);
+        }
+    }
+}
