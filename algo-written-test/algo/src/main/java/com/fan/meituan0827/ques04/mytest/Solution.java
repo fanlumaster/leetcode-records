3418f3c496f938778787d6b96b647712adb8a422
fanlumaster <1730976608@qq.com>
2022-08-31 22:53:24 +0800

    美团笔试 0827 第四题，没有完成。

---
+++ algo-written-test/algo/src/main/java/com/fan/meituan0827/ques04/mytest/Solution.java
@@ -0,0 +1,9 @@
+package com.fan.meituan0827.ques04.mytest;
+
+public class Solution {
+    public int getRes(int n, int b, int[] p, int[] t) {
+
+
+        return 0;
+    }
+}
---
+++ algo-written-test/algo/src/main/java/com/fan/meituan0827/ques04/solu/Main.java
@@ -0,0 +1,4 @@
+package com.fan.meituan0827.ques04.solu;
+
+public class Main {
+}
