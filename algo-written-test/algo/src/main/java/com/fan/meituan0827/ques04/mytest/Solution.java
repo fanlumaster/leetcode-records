81c4fcfa1c12f97c972a0e92fa04e83aa39bce39
fanlumaster <1730976608@qq.com>
2022-09-04 23:23:51 +0800

    清理门户。

--- algo-written-test/algo/src/main/java/com/fan/meituan0827/ques04/mytest/Solution.java
+++
@@ -1,9 +0,0 @@
-package com.fan.meituan0827.ques04.mytest;
-
-public class Solution {
-    public int getRes(int n, int b, int[] p, int[] t) {
-
-
-        return 0;
-    }
-}
--- algo-written-test/algo/src/main/java/com/fan/meituan0827/ques04/solu/Main.java
+++
@@ -1,4 +0,0 @@
-package com.fan.meituan0827.ques04.solu;
-
-public class Main {
-}
