988994d874e48ff76fb25198360e4670539c5f24
fanlumaster <1730976608@qq.com>
2022-09-27 02:40:17 +0800

    为了让工作在另一台笔记本上继续。

--- Cpp/run.ps1
+++ Cpp/run.ps1
@@ -5,6 +5,6 @@ $outputName=$filename.Substring($start, $strLength)
 $exeName=$outputName+".exe"
 # echo $filename
 # echo $exeName
-g++ $filename -o .\bin\$outputName
+g++ $filename -o .\bin\$outputName -I .\src\utils
 # echo 'compile completed and start execute:'
 & .\bin\$exeName
---
+++ Cpp/src/leet04/solu2.cpp
@@ -0,0 +1,22 @@
+// to make the time complexity equals to O(log(m+m))
+// we need to dichotomy
+#include <iostream>
+#include <vector>
+#include "containUtils.hpp"
+
+using namespace std;
+
+class Solution
+{
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        double res;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
