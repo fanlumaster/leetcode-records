121e0d5b4f38947bdd7481496eb0b8506012f415
fanlumaster <1730976608@qq.com>
2024-04-05 00:41:01 +0800

    optimize some scripts

--- Cpp/lrun.ps1
+++ Cpp/lrun.ps1
@@ -2,17 +2,17 @@
 $content = Get-Content -Path "./CMakeLists.txt"
 foreach($line in $content)
 {
-    if ($line.StartsWith("add_executable"))
+  if ($line.StartsWith("add_executable"))
+  {
+    $pattern = "\((.*?)\)"
+    if ($line -match $pattern)
     {
-        $pattern = "\((.*?)\)"
-        if ($line -match $pattern)
-        {
-            $contentInParentheses = $Matches[1]
-            $result = -split $contentInParentheses
-            $exePath = "./build/DEBUG/" + $result[0] + ".exe"
-            Write-Host "start running as follows..."
-            Write-Host "=================================================="
-            Invoke-Expression $exePath
-        }
+      $contentInParentheses = $Matches[1]
+      $result = -split $contentInParentheses
+      $exePath = "./build/DEBUG/" + $result[0] + ".exe"
+      Write-Host "start running as follows..."
+      Write-Host "=================================================="
+      Invoke-Expression $exePath
     }
+  }
 }
